431a490c714d1c2207940166abd2d7b2a1fb43ee
Agnytevs <mishasamodelkin856@gmail.com>
2026-03-10 11:33:39 +0500

    task_one.py

--- task_one.py
+++ task_one.py
@@ -1,17 +1,5 @@
-# Создаем первые две переменные и выводим их
-number_one = 0
-number_two = 1
-print(number_one)
-print(number_two)
-# Создаем цикл For, для вывода оставшихся 98 чисел
-for i in range(49):
-    # Выполняем вывод суммы чисел
+"""Этот код решает первую задачу."""
+number_one, number_two = 0, 1
+for _ in range(100):
     print(number_one + number_two)
-    # Обновляем переменные
-    number_three = number_one + number_two
-    number_one = number_two
-    #Выполняем вывод новой суммы чисел
-    print(number_one + number_three)
-    # Опять обновляем переменные
-    number_two = number_one + number_three
-    number_one = number_three
+    number_one, number_two = number_two, number_one + number_two
